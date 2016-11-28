25302f13ddc4ea850c012ee8d47142a151d5fb67
Brabant Scala User Group <brabant.scala@gmail.com>
2016-11-28 21:38:25 +0100

    Merging from original (#1)
    * Exercise 4.01 Skeleton and Unit Tests
    
    * Exercise 4.02 Unit Tests
    
    * Exercise 4.03 Unit Tests
    
    * Exercise 4.04 Unit Tests
    
    * Exercise 4.05 Unit Tests
    
    * Exercise 4.06 Unit Tests
    
    * Exercise 4.07 Unit Tests
    
    * Exercise 4.08 "Unit Tests"

---
+++ src/test/scala/nl/hugo/redbook/ch4/Test4_02.scala
@@ -0,0 +1,21 @@
+package nl.hugo.redbook.ch4
+
+import org.scalatest.{Matchers, WordSpec}
+
+import scala.{Either => _, Option => _, Some => _}
+
+class Test4_02 extends WordSpec with Matchers {
+  "variance" should {
+    "return Some(variance) for a list of values" in {
+      Option.variance(List(1.0, -1.0)) should be(Some(1.0))
+    }
+
+    "return Some(variance) of a list of a single value" in {
+      Option.variance(List(1.0)) should be(Some(0.0))
+    }
+
+    "return None for an empty list" in {
+      Option.variance(Nil) should be(None)
+    }
+  }
+}
---
+++ src/test/scala/nl/hugo/redbook/ch4/Test4_04.scala
@@ -0,0 +1,21 @@
+package nl.hugo.redbook.ch4
+
+import org.scalatest.{Matchers, WordSpec}
+
+import scala.{Either => _, Option => _, Some => _}
+
+class Test4_04 extends WordSpec with Matchers {
+  "sequence" should {
+    "transform a List(Some) into Some(List)" in {
+      Option.sequence(List(Some(1), Some(2), Some(3))) should be(Some(List(1,2,3)))
+    }
+
+    "transform a List(Some|None) into None" in {
+      Option.sequence(List(Some(1), None, Some(3))) should be(None)
+    }
+
+    "transform an emtpy list into Some(Nil)" in {
+      Option.sequence(Nil) should be(Some(Nil))
+    }
+  }
+}
