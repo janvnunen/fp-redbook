42ab4ddbabcf5cecdcf93aaeafd8d4060f4fe630
Tillaert <ardjan@gmail.com>
2016-12-19 23:32:04 +0100

    Commented out failing tests (because they require text answers)

--- src/test/scala/nl/hugo/redbook/ch4/Test4_08.scala
+++ src/test/scala/nl/hugo/redbook/ch4/Test4_08.scala
@@ -6,19 +6,19 @@ class Test4_08 extends WordSpec with Matchers {
 
   "map2" should {
     "What would you change in order to report both errors?" in {
-      ???
+
     }
 
     "What would you change map2 or the signature of mkPerson?" in {
-      ???
+
     }
 
     "Or could you create a new data type that captures this requirement better than Either does, with some additional sturcture?" in {
-      ???
+
     }
 
     "How would orElse, traverse, and sequence behave differently for that data type?" in {
-      ???
+
     }
   }
 }
