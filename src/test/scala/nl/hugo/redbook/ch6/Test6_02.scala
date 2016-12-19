5d20ad6afb99f7249452bd9f6ebe088b7ef0e72e
Tillaert <ardjan@gmail.com>
2016-12-19 23:26:35 +0100

    Improved tests.

--- src/test/scala/nl/hugo/redbook/ch6/Test6_02.scala
+++ src/test/scala/nl/hugo/redbook/ch6/Test6_02.scala
@@ -1,14 +1,33 @@
 package nl.hugo.redbook.ch6
 
-import org.scalatest.{ Matchers, WordSpec }
+import org.scalatest.{Matchers, WordSpec}
 
 class Test6_02 extends WordSpec with Matchers {
   "RNG.double" should {
+    val rng = RNG.Simple(0)
+
     "convert an integer to a double" in {
-      val rng = RNG.Simple(0)
       val mock = RNGMock(1234, rng)
 
-      RNG.double(mock) should be((0.000000574626028537750244140625, rng))
+      RNG.double(mock)._1 should be(0.000000574626028537750244140625 +- 1e-12)
+    }
+
+    "convert zero to a zero" in {
+      val mock = RNGMock(0, rng)
+
+      RNG.double(mock)._1 should be(0.0)
+    }
+
+    "convert IntMaxValue to a double" in {
+      val mock = RNGMock(Int.MaxValue, rng)
+
+      RNG.double(mock)._1 should be(0.9999999995343387 +- 1e-12)
+    }
+
+    "return the new RNG object" in {
+      val mock = RNGMock(0, rng)
+
+      RNG.double(mock)._2 should be(rng)
     }
   }
 }
