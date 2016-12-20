da421823a54aec4aec85a46ca2c0a1d3339eabf7
Tillaert <ardjan@gmail.com>
2016-12-20 10:14:22 +0100

    Exercise 6.07

--- src/main/scala/nl/hugo/redbook/ch6/State.scala
+++ src/main/scala/nl/hugo/redbook/ch6/State.scala
@@ -56,6 +56,7 @@ object RNG {
   // Exercise 6.06
   def map2[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = ???
 
+  // Exercise 6.07
   def sequence[A](fs: List[Rand[A]]): Rand[List[A]] = ???
 
   def flatMap[A, B](f: Rand[A])(g: A => Rand[B]): Rand[B] = ???
---
+++ src/test/scala/nl/hugo/redbook/ch6/Test6_07.scala
@@ -0,0 +1,25 @@
+package nl.hugo.redbook.ch6
+
+import org.scalatest._
+
+class Test6_07 extends WordSpec with Matchers {
+  val rng = RNG.Simple(0)
+
+  "RNG.sequence" should {
+    "sequence a List[RNG[A]] into a List[A]" in {
+      val ints = List(RNG.unit(1), RNG.unit(2), RNG.unit(3))
+
+      RNG.sequence(ints)(rng)._2 should be(List(1, 2, 3))
+    }
+
+    "progress the RNG object" in {
+      val (_, rng2) = rng.nextInt
+      val (_, rng3) = rng2.nextInt
+      val (_, rng4) = rng3.nextInt
+
+      val l = List(RNG.doubleViaMap, RNG.doubleViaMap, RNG.doubleViaMap)
+
+      RNG.sequence(l)(rng)._2 should be(rng4)
+    }
+  }
+}
