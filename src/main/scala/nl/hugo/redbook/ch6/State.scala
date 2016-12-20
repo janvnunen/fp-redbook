fc5dd0bbedbaf8ef45ef08f1330167865dc17b37
Tillaert <ardjan@gmail.com>
2016-12-20 09:30:12 +0100

    Exercise 6.06

--- src/main/scala/nl/hugo/redbook/ch6/State.scala
+++ src/main/scala/nl/hugo/redbook/ch6/State.scala
@@ -53,6 +53,7 @@ object RNG {
   // Exercise 6.05
   def doubleViaMap: Rand[Double] = ???
 
+  // Exercise 6.06
   def map2[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = ???
 
   def sequence[A](fs: List[Rand[A]]): Rand[List[A]] = ???
