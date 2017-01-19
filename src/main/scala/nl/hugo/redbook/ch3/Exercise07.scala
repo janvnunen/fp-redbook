79b1370371cfe862aadd5e03cfecf2c9b15391ab
Tillaert <ardjan@gmail.com>
2017-01-19 12:32:56 +0100

    Expanded answer.

--- src/main/scala/nl/hugo/redbook/ch3/Exercise07.scala
+++ src/main/scala/nl/hugo/redbook/ch3/Exercise07.scala
@@ -1,6 +1,15 @@
 package nl.hugo.redbook.ch3
 
 object Exercise07 {
+  /*
+  It is not possible with a regular foldRight. In
+
+  def foldRight[A,B](as: List[A], z: B)(f: (A, B): B
+
+  the arguments of f are evaluated before entering the function. Since the second argument is the foldRight result of
+   the next element, it forces the evaluation. Therefore the second argument must be passed by name, delaying evaluation.
+   */
+
   def foldRightLazy[A, B](as: List[A], z: B)(f: (A, => B) => B): B =
     as match {
       case Nil => z
