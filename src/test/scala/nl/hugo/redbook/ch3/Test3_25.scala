14de0e5bc7010e57986b8411572089c321e60d18
Ardjan Dommisse <Tillaert@users.noreply.github.com>
2017-01-19 14:30:52 +0100

    Test fix for issue #7. Branches should be counted too. (#8)

--- src/test/scala/nl/hugo/redbook/ch3/Test3_25.scala
+++ src/test/scala/nl/hugo/redbook/ch3/Test3_25.scala
@@ -7,7 +7,7 @@ class Test3_25 extends WordSpec with Matchers {
   "size" should {
 
     "return the number of leafs in a non-empty tree" in {
-      Exercise25.size(Branch(Branch(Leaf(1), Leaf(2)), Branch(Leaf(3), Leaf(4)))) should be(4)
+      Exercise25.size(Branch(Branch(Leaf(1), Leaf(2)), Branch(Leaf(3), Leaf(4)))) should be(7)
     }
 
     "return 1 for a tree with one element" in {
